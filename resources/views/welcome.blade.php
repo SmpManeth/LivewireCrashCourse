2b8a5005324f4e1df880049b3216c4ab87ac9cea
smpmaneth <smpmaneth@gmail.com>
2025-01-23 23:57:26 +0530

    Fisrt Component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LiveWire Crash Cord</title>
+    <title>LiveWire Crash Course</title>
 </head>
+
 <body>
-    <h1>Hello World</h1>
+    <livewire:first-component />
 </body>
+
 </html>
